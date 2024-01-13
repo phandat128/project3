5403132d3f47153e8620225fa2d8e9a2999fa51f
phandat128 <phandat12082002@gmail.com>
2024-01-13 16:33:07 +0700

    update

---
+++ experiments/vietnews/binarize_finetune_data.sh
@@ -0,0 +1,16 @@
+src=article
+tgt=abstract
+PROJ=.
+INPUT=$PROJ/data/vietnews
+OUTPUT=$INPUT/finetune-bin
+
+fairseq-preprocess \
+	--source-lang $src \
+	--target-lang $tgt \
+	--trainpref $INPUT/train.long.tok \
+	--validpref $INPUT/validation.short.tok,$INPUT/validation.long.tok \
+	--testpref $INPUT/test.short.tok,$INPUT/test.long.tok \
+	--tgtdict $INPUT/bin/dict.$tgt.txt
+	--srcdict $INPUT/bin/dict.$src.txt
+	--destdir $OUTPUT \
+	--workers 2
